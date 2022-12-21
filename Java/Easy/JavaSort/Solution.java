ea72468acb987ee73f4a3fb896f8869b80854fe2
meof-coding <tranthe150186@fpt.edu.vn>
2022-12-22 00:11:22 +0700

    Revert "[Print Prime Numbers] solved 100%"
    This reverts commit 7d9d735dccb5de11dddf529d3ddc75776c9af41f.

--- Java/Easy/JavaSort/Solution.java
+++ Java/Easy/JavaSort/Solution.java
@@ -2,53 +2,52 @@ package Java.Easy.JavaSort;
 
 import java.util.*;
 
-class Student {
-    private int id;
-    private String fname;
-    private double cgpa;
-
-    public Student(int id, String fname, double cgpa) {
-        super();
-        this.id = id;
-        this.fname = fname;
-        this.cgpa = cgpa;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public String getFname() {
-        return fname;
-    }
-
-    public double getCgpa() {
-        return cgpa;
-    }
+class Student{
+	private int id;
+	private String fname;
+	private double cgpa;
+	public Student(int id, String fname, double cgpa) {
+		super();
+		this.id = id;
+		this.fname = fname;
+		this.cgpa = cgpa;
+	}
+	public int getId() {
+		return id;
+	}
+	public String getFname() {
+		return fname;
+	}
+	public double getCgpa() {
+		return cgpa;
+	}
 }
 
 //Complete the code
-public class Solution {
-    public static void main(String[] args) {
-        int i = 2;
-        int prime = 0;
-        List<Integer> primes = new ArrayList<Integer>();
-        while (i < 1000) {
-            int j = i - 1;
-            prime = 1;
-            while (j > 1) {
-                if (i % j == 0) {
-                    prime = 0;
-                    break;
-                }
-                j--;
-            }
-            if (prime == 1) {
-                primes.add(i);
-            }
-            i++;
-        }
-    }
+public class Solution
+{
+	public static void main(String[] args){
+		Scanner in = new Scanner(System.in);
+		int testCases = Integer.parseInt(in.nextLine());
+		
+		List<Student> studentList = new ArrayList<Student>();
+		while(testCases>0){
+			int id = in.nextInt();
+			String fname = in.next();
+			double cgpa = in.nextDouble();
+			
+			Student st = new Student(id, fname, cgpa);
+			studentList.add(st);
+			
+			testCases--;
+		}
+
+		studentList.sort(Comparator.comparing(Student::getCgpa).reversed().thenComparing(Student::getFname).thenComparing(Student::getId));
+      
+      	for(Student st: studentList){
+			System.out.println(st.getFname());
+		}
+	}
 }
 
 
