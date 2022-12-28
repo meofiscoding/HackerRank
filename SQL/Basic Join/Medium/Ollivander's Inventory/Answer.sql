455583761df3c2a50900ad2e49db84678e421afb
meof-coding <tranthe150186@fpt.edu.vn>
2022-12-29 00:34:26 +0700

    Update Answer.sql

--- SQL/Basic Join/Medium/Ollivander's Inventory/Answer.sql
+++ SQL/Basic Join/Medium/Ollivander's Inventory/Answer.sql
@@ -1,15 +1,12 @@
-select c.company_code, c.founder, count(distinct l.lead_manager_code),count(distinct s.senior_manager_code),count(distinct m.manager_code),count(distinct e.employee_code) from company c
-inner join
-lead_manager l
-on c.company_code = l.company_code
-inner join 
-senior_manager s
-on c.company_code = s.company_code
-inner join
-manager m
-on c.company_code = m.company_code
-inner join
-employee e
-on c.company_code = e.company_code
-group by c.company_code, c.founder
-order by c.company_code
+-- Get min(coin_needed) for each Wands group by [power],code and not evil
+with a as(select w.code, wp.age, min(w.coins_needed) as coins, w.power from Wands w 
+inner join Wands_Property wp 
+on w.code = wp.code 
+where wp.is_evil = 0 
+group by w.power,wp.age , w.code
+)
+-- Join with Wands to get id
+select w.id, a.age, a.coins, a.power from a inner join Wands w on a.code = w.code and a.coins = w.coins_needed and a.power = w.power
+
+-- Order by Stuff
+order by a.power DESC, a.age DESC
