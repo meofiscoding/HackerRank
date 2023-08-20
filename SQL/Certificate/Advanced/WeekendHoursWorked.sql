b01e0b69f97f9414f795165171c448c98bb69441
meof-coding <tranthe150186@fpt.edu.vn>
2023-08-20 23:01:29 +0700

    Update WeekendHoursWorked.sql

--- SQL/Certificate/Advanced/WeekendHoursWorked.sql
+++ SQL/Certificate/Advanced/WeekendHoursWorked.sql
@@ -1,14 +1,31 @@
 SET NOCOUNT ON;
 
+WITH WorkHours AS (
     SELECT 
-        emp_id, CAST([TIMESTAMP] AS DATE)
-        -- datediff(HOUR, MIN([TIMESTAMP]), MAX([TIMESTAMP])) as [Hours Worked]
-        FROM  ATTENDANCE
-        WHERE
-            DATEPART(DW, [TIMESTAMP]) IN (1, 7)
-        group by 
-            emp_id
-    
+        emp_id,
+        CAST([TIMESTAMP] AS DATE) AS work_date,
+        DATEDIFF(HOUR, MIN([TIMESTAMP]), MAX([TIMESTAMP])) as [Hours Worked]
+    FROM ATTENDANCE
+    WHERE DATEPART(DW, [TIMESTAMP]) IN (1, 7)
+    GROUP BY emp_id, CAST([TIMESTAMP] AS DATE)
+)
+SELECT
+    emp_id,
+    SUM([Hours Worked]) AS [Total Hours Worked]
+FROM WorkHours
+GROUP BY emp_id
+order by [Total Hours Worked]
+
 GO
 
+-- i think this is the correct version
+-- SET NOCOUNT ON;
+-- SELECT 
+--     emp_id,
+--     SUM(DATEDIFF(HOUR, MIN([TIMESTAMP]), MAX([TIMESTAMP]))) as [Total Hours Worked]
+-- FROM  ATTENDANCE
+-- WHERE
+--     DATEPART(DW, [TIMESTAMP]) IN (1, 7)
+-- GROUP BY emp_id;
 
+-- GO
