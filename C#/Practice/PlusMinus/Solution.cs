ba10e141dfe7fccb38c2c1ca165fec862ffc8b7c
meof-coding <tranthe150186@fpt.edu.vn>
2023-08-22 15:03:57 +0700

    feat: [C# Practice] Plus Minus

---
+++ C#/Practice/PlusMinus/Solution.cs
@@ -0,0 +1,44 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'plusMinus' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static void plusMinus(List<int> arr)
+    {
+        // take 6 decimal places
+        Console.WriteLine("{0:0.000000}", arr.Count(x => x > 0) / (double)arr.Count);
+        Console.WriteLine("{0:0.000000}", arr.Count(x => x < 0) / (double)arr.Count);
+        Console.WriteLine("{0:0.000000}", arr.Count(x => x == 0) / (double)arr.Count);
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        Result.plusMinus(arr);
+    }
+}
