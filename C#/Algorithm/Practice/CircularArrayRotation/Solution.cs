22941eeb3d00882a6f4136c6046ede4b191bf96c
meof-coding <tranthe150186@fpt.edu.vn>
2023-08-23 11:59:07 +0700

    feat: [C# Practice] Circular Array Rotation

---
+++ C#/Algorithm/Practice/CircularArrayRotation/Solution.cs
@@ -0,0 +1,71 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'circularArrayRotation' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY a
+     *  2. INTEGER k
+     *  3. INTEGER_ARRAY queries
+     */
+
+    public static List<int> circularArrayRotation(List<int> a, int k, List<int> queries)
+    {
+        // using reverse operations to rotate array k times 
+        // https://www.geeksforgeeks.org/reversal-algorithm-right-rotation-array/
+        k = k % a.Count;
+        a.Reverse();
+        a.Reverse(0, k);
+        a.Reverse(k, a.Count - k);
+        return queries.Select(x => a[x]).ToList();
+    }
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        //TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+        int n = Convert.ToInt32(firstMultipleInput[0]);
+
+        int k = Convert.ToInt32(firstMultipleInput[1]);
+
+        int q = Convert.ToInt32(firstMultipleInput[2]);
+
+        List<int> a = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(aTemp => Convert.ToInt32(aTemp)).ToList();
+
+        List<int> queries = new List<int>();
+
+        for (int i = 0; i < q; i++)
+        {
+            int queriesItem = Convert.ToInt32(Console.ReadLine().Trim());
+            queries.Add(queriesItem);
+        }
+
+        List<int> result = Result.circularArrayRotation(a, k, queries);
+
+        //textWriter.WriteLine(String.Join("\n", result));
+
+        //textWriter.Flush();
+        //textWriter.Close();
+    }
+}
