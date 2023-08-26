a3a7f0c70c6117d4347d7e0e1daa39792839ab6a
meof-coding <tranthe150186@fpt.edu.vn>
2023-08-27 00:43:21 +0700

    c#: Recursive Digit Sum

---
+++ C#/Algorithm/Practice/RecursiveDigitSum/Solution.cs
@@ -0,0 +1,65 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'superDigit' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. STRING n
+     *  2. INTEGER k
+     */
+
+    public static int superDigit(string n, int k)
+    {
+        if (n.Length == 1)
+        {
+            return Convert.ToInt32(n);
+        }
+        else
+        {
+            long sum = 0;
+            for (int i = 0; i < n.Length; i++)
+            {
+                sum += Convert.ToInt32(n[i].ToString());
+            }
+            return superDigit((sum * k).ToString(), 1);
+        }
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        //TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+        string n = firstMultipleInput[0];
+
+        int k = Convert.ToInt32(firstMultipleInput[1]);
+
+        int result = Result.superDigit(n, k);
+
+        //textWriter.WriteLine(result);
+
+        //textWriter.Flush();
+        //textWriter.Close();
+    }
+}
